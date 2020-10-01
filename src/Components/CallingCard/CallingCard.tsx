7a25688dfc6d41c8b0521810a0b912b9483bf17b
Daniel Sottile <danielsottile@gmail.com>
2020-10-01 13:08:07 -0400

    format

--- src/Components/CallingCard/CallingCard.tsx
+++ src/Components/CallingCard/CallingCard.tsx
@@ -7,13 +7,13 @@ const calc = (x: any, y: any) => [
   x - window.innerWidth / 2,
   y - window.innerHeight / 2,
 ];
-const trans1 = (x: any, y: any) => `translate3d(${x / 50}px,${y / 50}px,0)`;
-const trans2 = (x: any, y: any) => `translate3d(${x / 80}px,${y / 80}px,0)`;
-const trans3 = (x: any, y: any) => `translate3d(${x / 30}px,${y / 30}px,0)`;
-const trans4 = (x: any, y: any) =>
-  `translate3d(${x / 23.25}px,${y / 23.25}px,0)`;
-const trans5 = (x: any, y: any) => `translate3d(${x / 17.5}px,${y / 17.5}px,0)`;
-const trans6 = (x: any, y: any) => `translate3d(${x / 7.5}px,${y / 7.5}px,0)`;
+const trans1 = (x: any, y: any) => `translate3d(${x / 80}px,${y / 80}px,0)`;
+const trans2 = (x: any, y: any) => `translate3d(${x / 25}px,${y / 25}px,0)`;
+const trans3 = (x: any, y: any) => `translate3d(${x / 15}px,${y / 15}px,0)`;
+const trans4 = (x: any, y: any) => `translate3d(${x / 30}px,${y / 30}px,0)`;
+const trans5 = (x: any, y: any) => `translate3d(${x / 23}px,${y / 23}px,0)`;
+const trans6 = (x: any, y: any) => `translate3d(${x / 17.5}px,${y / 17.5}px,0)`;
+const trans7 = (x: any, y: any) => `translate3d(${x / 7.5}px,${y / 7.5}px,0)`;
 
 const CallingCard = (): JSX.Element => {
   const [props, set] = useSpring(() => ({
@@ -51,6 +51,10 @@ const CallingCard = (): JSX.Element => {
           <animated.div
             className="Ccard6"
             style={{ transform: props.xy.interpolate(trans6 as any) }}
+          />
+          <animated.div
+            className="Ccard7"
+            style={{ transform: props.xy.interpolate(trans7 as any) }}
           />
         </div>
       </ScrollAnimation>
